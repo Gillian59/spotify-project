7433131e056940692dc80aa40367c075521c0003
TheRealAstoo <louis.fanien@gmail.com>
2020-11-18 17:34:32 +0100

    undone some suppressions made to fast

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import NavSideBar from "./NavSideBar";
-import TracksList from "./TracksList";
 
 const MainContainer: React.FC = ({ children }) => {
   return (
     <>
       <div className="App">
         <NavSideBar />
-        <TracksList tracksNamesList={["piste 1", "piste 2", "piste 3"]} />
       </div>
       {children}
     </>
